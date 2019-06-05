555852dd135fe304cde10a57346cbb3eb45d0b11
Adam Humpherys <adam@adamh.us>
2019-06-05 10:20:04 -0600

    Initial source commit

---
+++ src/BillingOrchardApi/BillingOrchardRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BillingOrchardApi
+{
+    public class BillingOrchardRequest
+    {
+        internal string _key { get; }
+        internal string _service { get; }
+        internal const string _hmacKey = "BillingOrchard2012";
+
+        public BillingOrchardRequest(string key, string service)
+        {
+            _key = key;
+            _service = service;
+        }
+
+        internal string GenerateHmac()
+        {
+            string signature = _key + _service + DateTime.UtcNow.ToString("yyyyMMdd HH:mm");
+            var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(_hmacKey));
+            return Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(signature)));
+        }
+
+
+    }
+}
